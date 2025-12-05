aedeebcd36a28484f2bcaa5ec161c06a09a3b791
Gaurav Vaidya <gaurav@renci.org>
2025-12-05 18:25:48 -0500

    Added Redis version argument.

--- data-loading/rdb-to-resp.sh
+++ data-loading/rdb-to-resp.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #
 # A Bash script to convert an RDB file into a stream of RESP commands.
+# Uses https://github.com/redis/librdb?tab=readme-ov-file#rdb-cli-usage
 #
-# bash rdb-to-resp.sh RDB_FILENAME
+# bash rdb-to-resp.sh RDB_FILENAME [REDIS_VERSION]
 set -euo pipefail
 
 # Check the number of arguments
@@ -10,5 +11,6 @@ if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <RDB_FILENAME>" >&2
     exit 1
 fi
+REDIS_VERSION="${2:-6.2}" # Default to a Redis version of 6.2
 
-rdb-cli "$1" -l stdout -s 1000 -d 0 resp
+rdb-cli "$1" --show-progress 1000 resp --target-redis-ver "${REDIS_VERSION}"
